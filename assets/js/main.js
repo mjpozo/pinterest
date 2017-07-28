369cf6fe332526899dfe4a15db59d261855211db
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-28 17:44:31 -0400

    maqueta del navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,6 @@
 $(document).ready(function(){
 
 	data.forEach(function(element){
-		$(".container").append('<p>id: ' + element.id + '</p>' + '<p>title: ' + element.title + '</p>' + '<p>description: ' + element.description + '</p>' + '<p>user: ' + element.user + '</p>' + '<p>img: ' + element.img + '</p>');
+		$(".container").append('<p>id: ' + element.id + '</p>' + '<p>title: ' + element.title + '</p>' + '<p>description: ' + element.description + '</p>' + '<p>user: ' + element.user + '</p>' + '<img src="../../dist/img/' + element.image_url + '"></img>');
 	});
 })
