d304b2ab88237422d61125019c4ef4393e9bad22
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-29 03:07:05 -0400

    con flexbox se establece la estructura de columnas

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,16 @@
 $(document).ready(function(){
 
 	data.forEach(function(element){
-		$(".container").append('<p>id: ' + element.id + '</p>' + '<p>title: ' + element.title + '</p>' + '<p>description: ' + element.description + '</p>' + '<p>user: ' + element.user + '</p>' + '<img src="../../dist/img/' + element.image_url + '"></img>');
+		$(".container").append('<div class="pin"><a id="' + element.id + '"><img src="../../dist/img/' + element.image_url + '" class="responsive"></img></a>' + '<p>title: ' + element.title + '</p>' + '<p>description: ' + element.description + '</p>' + '<p>user: ' + element.user + '</p>' + '</div>');
+		
+
+		$("#" + element.id).click(function(){
+			$(".modal-info").empty();
+			$(".modal-info").append('<p id="' + element.id + '">id: ' + element.id + '</p>');
+			$(".modal").show();
+		});
+		$("span").click(function(){
+			$(".modal").hide();
+		});
 	});
 })
